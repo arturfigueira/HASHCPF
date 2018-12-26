a7c755e5797151bcffbf196de43c5fca429e688f
arturfigueira <arturfigueira@gmail.com>
2018-12-26 10:23:44 -0200

    Project Structure created with gradle

---
+++ src/main/java/com/ca/datamasker/custom/HashCPF.java
@@ -0,0 +1,11 @@
+package com.ca.datamasker.custom;
+
+import com.grid_tools.products.datamasker.IMaskFunction;
+
+public class HashCPF implements IMaskFunction {
+
+    @Override
+    public Object mask(Object... objects) {
+        return null;
+    }
+}
