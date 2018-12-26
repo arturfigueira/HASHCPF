82428b1a204843751eb94fa8f737e420f33bb86d
arturfigueira <arturfigueira@gmail.com>
2018-12-26 20:51:55 -0200

    Hash method with test

--- src/main/java/com/ca/datamasker/custom/HashCPF.java
+++ src/main/java/com/ca/datamasker/custom/HashCPF.java
@@ -1,11 +1,38 @@
 package com.ca.datamasker.custom;
 
+import com.grid_tools.products.datamasker.Datamasker;
 import com.grid_tools.products.datamasker.IMaskFunction;
+import com.grid_tools.products.datamasker.randfunctions;
 
 public class HashCPF implements IMaskFunction {
 
     @Override
     public Object mask(Object... objects) {
-        return null;
+        final String cpfToBeMasked = (String) objects[0];
+        String maskedValue = cpfToBeMasked;
+        if(cpfToBeMasked != null && !cpfToBeMasked.isEmpty()){
+
+            try{
+                maskedValue = hashCPF(cpfToBeMasked);
+            }catch (IllegalArgumentException e){
+                Datamasker.processOutputs(Datamasker.formatMessage("m0345-hasherr", new String[] { "HASH" }));
+                Datamasker.processErrors(Datamasker.formatMessage("m0345-hasherr", new String[] { "HASH" }));
+                Datamasker.processOutputs(Datamasker.formatMessage("m0155-DBValue", new String[] { cpfToBeMasked }));
+                System.exit(1);
+            }
+        }
+
+        return maskedValue;
+    }
+
+    protected String hashCPF(final String cpf){
+        if(!CPFValidator.isValid(cpf)){
+            throw new IllegalArgumentException("This CPF is invalid, for this reason It cant be Masked");
+        }
+
+        final String number = cpf.substring(0, cpf.length() - 2);
+        final String hashedValue = randfunctions.formatHash(number);
+        final CPF hashedCPF = new CPF(hashedValue);
+        return hashedCPF.toString();
     }
 }
---
+++ src/test/java/com/ca/datamasker/custom/HashCPFTest.java
@@ -0,0 +1,43 @@
+package com.ca.datamasker.custom;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HashCPFTest {
+
+    private HashCPF hasher;
+
+    @Before
+    public void setUp() throws Exception {
+        hasher = new HashCPF();
+    }
+
+    @Test
+    public void testHashedCPFIsValid() {
+        final String validCPF = new MockCPFFactory().getACPF();
+        final String hashedCPF = hasher.hashCPF(validCPF);
+        assertTrue("Hashing a valid CPF should generate a valid CPF too", CPFValidator.isValid(hashedCPF));
+    }
+
+    @Test
+    public void testHasherMaintainIntegry() {
+        final String validCPF = new MockCPFFactory().getACPF();
+        final String hashedCPF = hasher.hashCPF(validCPF);
+        final String hashedAgainCPF = hasher.hashCPF(validCPF);
+        assertEquals("Hashing value twice should return same hashed value", hashedCPF, hashedAgainCPF);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidSmallLength() {
+        final String validCPF = new MockCPFFactory(FormatMask.NOTMASK, CPFSize.SMALLER).getACPF();
+        hasher.hashCPF(validCPF);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidLargeLength() {
+        final String validCPF = new MockCPFFactory(FormatMask.NOTMASK, CPFSize.LARGER).getACPF();
+        hasher.hashCPF(validCPF);
+    }
+}
